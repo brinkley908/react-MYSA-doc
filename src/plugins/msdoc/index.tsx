bb7d2aa3f19d687f3d18d3656b5e3a9f1a63c995
Richard Brinkley <brinkley908@gmail.co.uk>
2021-06-08 20:23:48 +0100

    gjhjhg

--- src/plugins/msdoc/index.tsx
+++ src/plugins/msdoc/index.tsx
@@ -8,7 +8,7 @@ const MSDocRenderer: DocRenderer = ({ mainState: { currentDocument } }) => {
   return (
     <Container id="msdoc-renderer">
       <IFrame
-       sandbox="allow-scripts"
+        sandbox=""
         id="msdoc-iframe"
         title="msdoc-iframe"
         src={`https://view.officeapps.live.com/op/embed.aspx?src=${encodeURIComponent(
