69d93fafaded40a21816a4ff195c4813b1a9fb31
Richard Brinkley <brinkley908@gmail.co.uk>
2021-06-08 18:15:47 +0100

    ghghjjh

--- src/utils/fileLoaders.ts
+++ src/utils/fileLoaders.ts
@@ -18,7 +18,7 @@ const _fileLoader: BaseFileLoaderFunction = ({
   fileLoaderComplete,
   readerTypeFunction,
 }) => {
-  return fetch(documentURI, { signal, mode: 'no-cors' })
+  return fetch(documentURI, { signal, mode: 'no-cors', headers: {"Access-Control-Allow-Origin": "*", "Access-Control-Allow-Headers": "X-Requested-With"} })
     .then(async (res) => {
       const blob = await res.blob();
 
